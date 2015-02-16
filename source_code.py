dcffd9c42368039b44a41452073f7765b5492605
peterZuz <peter_zuzin@mail.ru>
2015-02-16 23:47:07 +0300

    second version

--- source_code.py
+++ source_code.py
@@ -9,36 +9,27 @@ def get_data_from_xml(xml_file_name):
         elem_tree.parse(xml_file)
 
     num_net = len(elem_tree.getroot().findall('net'))
-    start_matrix = [[float(0) for j in range(num_net)] for i in range(num_net)]
+    start_matrix = [[float('inf') for j in range(num_net)] for i in range(num_net)]
 
     for d in elem_tree.getroot().findall('diode'):
             i = int(d.attrib['net_from']) - 1
             j = int(d.attrib['net_to']) - 1
-            start_matrix[i][j] = d.attrib['resistance']
-            start_matrix[j][i] = d.attrib['reverse_resistance']
+            start_matrix[i][j] = 1/(1/start_matrix[i][j] + 1/float(d.attrib['resistance']))
+            start_matrix[j][i] = 1/(1/start_matrix[j][i] + 1/float(d.attrib['reverse_resistance']))
 
     for r in elem_tree.getroot().findall('resistor'):
             i = int(r.attrib['net_from']) - 1
             j = int(r.attrib['net_to']) - 1
-            start_matrix[i][j] = r.attrib['resistance']
-            start_matrix[j][i] = r.attrib['resistance']
+            start_matrix[i][j] = 1/(1/start_matrix[i][j] + 1/float(r.attrib['resistance']))
+            start_matrix[j][i] = 1/(1/start_matrix[j][i] + 1/float(r.attrib['resistance']))
 
     for c in elem_tree.getroot().findall('capactor'):
             i = int(c.attrib['net_from']) - 1
             j = int(c.attrib['net_to']) - 1
-            start_matrix[i][j] = c.attrib['resistance']
-            start_matrix[j][i] = c.attrib['resistance']
+            start_matrix[i][j] =  1/(1/start_matrix[i][j] + 1/float(c.attrib['resistance']))
+            start_matrix[j][i] =  1/(1/start_matrix[j][i] + 1/float(c.attrib['resistance']))
 
     return num_net, start_matrix
-
-
-def export_data_to_csv(csv_file_name, size, matrix):
-    with open(csv_file_name, 'w') as csv_file:
-        for i in range(size):
-            for j in range(size):
-                print("%.6f" % (matrix[i, j]), end=',', file=csv_file)
-            print('\n', end='', file=csv_file)
-    return
 
 
 def resist_calculation(num_net, start_matrix):
@@ -51,7 +42,7 @@ def resist_calculation(num_net, start_matrix):
             try:
                 final_matrix[i][j] = 1/(1/final_matrix[i][j] + 1/start_matrix[i][j])
             except ZeroDivisionError:
-                if start_matrix[i][j] == 0 | final_matrix[i][j] == 0:
+                if start_matrix[i][j] == 0 or final_matrix[i][j] == 0:
                     final_matrix[i][j] = 0
 
     for k in range(num_net):
@@ -60,10 +51,19 @@ def resist_calculation(num_net, start_matrix):
                 try:
                     final_matrix[i][j] = 1/(1/final_matrix[i][j] + 1/(final_matrix[i][k] + final_matrix[k][j]))
                 except ZeroDivisionError:
-                    if final_matrix[i][j] == 0 | final_matrix[i][k] == 0 | final_matrix[k][j] == 0:
+                    if final_matrix[i][j] == 0 or final_matrix[i][k] == 0 or final_matrix[k][j] == 0:
                         final_matrix[i][j] = 0
 
     return final_matrix
+
+
+def export_data_to_csv(csv_file_name, size, matrix):
+    with open(csv_file_name, 'w') as csv_file:
+        for i in range(size):
+            for j in range(size):
+                print("%.6f" % (matrix[i][j]), end=',', file=csv_file)
+            print('\n', end='', file=csv_file)
+    return
 
 
 def main_func(file_input, file_output):
@@ -75,7 +75,7 @@ def main_func(file_input, file_output):
 
     finish_t = time.time()
 
-    return finish_t - start_t
+    return (finish_t - start_t) * 1000
 
 
 if __name__ == "__main__":
